66b799e184270cd10fbf8ddd18fca2d3868750e8
Andrew Janssen <janssenandrew1@gmail.com>
2020-01-20 11:21:11 -0600

    css file, more index

--- index.php
+++ index.php
@@ -3,10 +3,21 @@
 
 <!DOCTYPE HTML>
 <html>
+
 <head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <title>Task List</title>
 </head>
 
+<body>
+  <h1 class='heading'>Task List App</h1>
+  <div>
+    <input type='text' name='task' id='input' placeholder='Enter a task' />
+    <button id='button' type='submit' name='add'> Add </button>
+  </div>
 
+  <ul id='container'> </ul>
+</body>
 
 </html>

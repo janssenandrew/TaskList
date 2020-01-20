4f05228bdef4f9c38fdb02c4d2e4631472db0d60
Andrew Janssen <janssenandrew1@gmail.com>
2020-01-20 11:53:09 -0600

    alert for empty task

--- handler.js
+++ handler.js
@@ -29,35 +29,40 @@ function init(str) {
 }
 
 function add() {
-    jQuery.ajax({
-        type: "POST",
-        url: 'api/add.php',
-        data: {
-            value: document.getElementById('input').value
-        },
-    }).done(function(data) {
-        document.getElementById('input').value = '';
-        let js = JSON.parse(data);
-        var container = document.getElementById('container');
-        var li = document.createElement('li');
-        var box = document.createElement('input');
-        box.addEventListener("click", () => {
-            update(js.id)
+    const task = document.getElementById('input').value;
+    if (task == '')
+        alert('Cannot enter an empty task');
+    else {
+        jQuery.ajax({
+            type: "POST",
+            url: 'api/add.php',
+            data: {
+                value: document.getElementById('input').value
+            },
+        }).done(function(data) {
+            document.getElementById('input').value = '';
+            let js = JSON.parse(data);
+            var container = document.getElementById('container');
+            var li = document.createElement('li');
+            var box = document.createElement('input');
+            box.addEventListener("click", () => {
+                update(js.id)
+            });
+            var span = document.createElement('span');
+            span.innerHTML = js.task;
+            box.setAttribute('type', 'checkbox');
+            li.setAttribute("id", js.id);
+            li.appendChild(span);
+            li.appendChild(box);
+            container.appendChild(li);
         });
-        var span = document.createElement('span');
-        span.innerHTML = js.task;
-        box.setAttribute('type', 'checkbox');
-        li.setAttribute("id", js.id);
-        li.appendChild(span);
-        li.appendChild(box);
-        container.appendChild(li);
-    });
+    }
 }
 
 function update(num) {
     jQuery.ajax({
         type: "POST",
-        url: 'update.php',
+        url: 'api/update.php',
         data: {
             id: num
         },
